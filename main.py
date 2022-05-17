73174ed37bc3dc4c643baafbd7d74985154e7166
Gustavo Valladolid <a6520150033@utch.edu.mx>
2022-05-17 12:39:14 -0600

    Clean version

--- main.py
+++ main.py
@@ -4,13 +4,27 @@
 @descrip EJercicio de Flask
 """
 # Inclusion de modulos
-from flask import Flask
+from flask import Flask, request
 
 app = Flask('__main__')
 
-@app.route('/test', methods=['GET'])
-def go_home():
-    return 'Hello World!'
+user = {
+    "code":"112233",
+    "descrip":"Temp. Sensor",
+    "value": 67
+}
+
+
+@app.route('/users', methods=['GET'])
+def get_user():
+    return user   
+
+#Save an user
+@app.route('/users', methods=['POST'])
+def post_user():
+    user = request.json
+    print(user)
+    return user
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
